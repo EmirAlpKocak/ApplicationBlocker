f66cd2a87af3f25c9bb16281d294d877ab4cc9c3
EmirAlpKocak <168924880+EmirAlpKocak@users.noreply.github.com>
2026-04-27 22:19:53 +0300

    Add files via upload

---
+++ Protect-Process/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Protect_Process
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Hide();
+            this.Visible = false;
+            this.WindowState = FormWindowState.Minimized;
+            this.Opacity = 0;
+            if (Environment.Is64BitOperatingSystem)
+            {
+                CheckFile64();
+            }
+            else
+            {
+                CheckFile32();
+            }
+        }
+        private void CheckFile32()
+        {
+            try
+            {
+                FileStream stream = new FileStream("C:\\Program Files\\Application Blocker\\Application Blocker.exe", FileMode.Open, FileAccess.Read, FileShare.Read);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error has occured. The Protect-Process will be closed. Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(-1);
+            }
+        }
+        private void CheckFile64()
+        {
+            try
+            {
+                FileStream stream = new FileStream("C:\\Program Files (x86)\\Application Blocker\\Application Blocker.exe", FileMode.Open, FileAccess.Read, FileShare.Read);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error has occured. The Protect-Process will be closed. Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(-1);
+            }
+        }
+    }
+}
