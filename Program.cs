6df6604c66c54bb5ac7813c1f5eef296e8416274
EmirAlpKocak <168924880+EmirAlpKocak@users.noreply.github.com>
2026-04-27 22:16:29 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Application_Blocker
@@ -10,9 +11,17 @@ namespace Application_Blocker
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        static Mutex mutex;
         [STAThread]
         static void Main(string[] args)
         {
+            bool createdNew;
+            mutex = new Mutex(true, "ApplicationBlocker_MUTEX", out createdNew);
+            if (!createdNew)
+            {
+                MessageBox.Show("Another instance is already running. Check application tray icon to access.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             bool startup = args.Contains("/startup");
